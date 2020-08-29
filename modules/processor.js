7626fb801b7d5c9c0853b647b75a6cdfb9bfd853
Tilman Moser <tilman.moser@impavit.com>
2020-08-29 17:18:08 +0200

    Changed workingdir to inputdir/assets

--- modules/processor.js
+++ modules/processor.js
@@ -37,7 +37,7 @@ class Processor {
 
     async createAssets() {
         try {
-            this.workdir = './tmp/' + this.metadata.id
+            this.workdir = this.inputDir + '/assets'
             await createWorkdir(this.workdir)
             this.shapeFiles = await createShapes(this.shapes, this.workdir),
             this.captionFiles = await createCaptions(this.captions, this.inputDir, this.workdir)
